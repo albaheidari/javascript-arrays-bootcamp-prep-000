d5a1a5b1a5e9153bb6a660ba0d6007e1bcf408c1
Alba Heidari <albaheidari@gmail.com>
2018-08-30 09:20:20 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,45 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat","skittles"];
+
+function addElementToBeginningOfArray(array,element){
+  var elements =[1];
+  var elements2 = ["foo", ...elements];
+  return(elements2);
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+array.unshift("foo");
+return(array);
+}
+
+function addElementToEndOfArray(array,element){
+  var elements =[1];
+  var elements2 = [...elements,"foo"];
+  return(elements2);
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+array.push("foo");
+return(array);
+}
+
+function accessElementInArray(array, index) {
+  var arrays =[1, 2, 3]
+  return array[2];
+
+}
+function destructivelyRemoveElementFromBeginningOfArray(array){ 
+  array.shift(); 
+  return array;
+}
+function removeElementFromBeginningOfArray(array) {
+var array1 = [1, 2, 3];
+var array2 = array1.slice(1);
+return array2;
+}
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop();
+  return array;
+}
+function removeElementFromEndOfArray(array) {
+  
+}
